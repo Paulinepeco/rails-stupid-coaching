7a4b48099c12d7322575c66b82e054905a48466a
Pauline Pecorini <pauline.pecorini@gmail.com>
2016-08-08 14:34:18 +0200

    fini

--- app/controllers/coaching_controller_controller.rb
+++ app/controllers/coaching_controller_controller.rb
@@ -1,10 +1,21 @@
 class CoachingControllerController < ApplicationController
   def answer
     @query = params[:query]
-
+    @answer = coach_answer(@query)
   end
 
   def ask
     @query = params[:query]
   end
+
+  def coach_answer(your_message)
+    if your_message.downcase == "i am going to work right now!"
+      ''
+    elsif your_message.end_with?("?")
+      "Silly question, get dressed and go to work!"
+    else
+      "I don't care, get dressed and go to work!"
+    end
+  end
+
 end
